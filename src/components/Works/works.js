f1dcd2e08f66a776557989b65a76ceff984b63fa
manu-mishra-cloud <8manoranjan@gmail.com>
2024-12-01 11:55:42 +0530

    test

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -1,6 +1,8 @@
 import React from "react";
 import './works.css';
 import PortfolioImg from '../../assets/portfolio.jpg'
+import PortfolioImg1 from '../../assets/port1.gif'
+import PortfolioImg2 from '../../assets/port2.gif'
 
 const Works = () => {
     return (
@@ -8,12 +10,12 @@ const Works = () => {
             <h2 className="worksTitle">My Portfolio</h2>
             <span className="worksDesc">A glimpse into my work, showcasing my passion for design and attention to detail. Browse through these projects to see my skills in action and the quality of my design work.</span>
             <div className="worksImgs">
-                <img src={PortfolioImg} alt="" className="worksImg" />
-                <img src={PortfolioImg} alt="" className="worksImg" />
-                <img src={PortfolioImg} alt="" className="worksImg" />
-                <img src={PortfolioImg} alt="" className="worksImg" />
-                <img src={PortfolioImg} alt="" className="worksImg" />
-                <img src={PortfolioImg} alt="" className="worksImg" />
+                <img src={PortfolioImg1} alt="" className="worksImg" />
+                <img src={PortfolioImg2} alt="" className="worksImg" />
+                <img src={PortfolioImg1} alt="" className="worksImg" />
+                <img src={PortfolioImg1} alt="" className="worksImg" />
+                <img src={PortfolioImg1} alt="" className="worksImg" />
+                <img src={PortfolioImg1} alt="" className="worksImg" />
             
             </div>
             <button className="workBtn">See More</button>
